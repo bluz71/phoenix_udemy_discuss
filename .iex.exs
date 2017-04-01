a1d0e539d5c54d6c97561231973aee2ec7d6dcb4
Dennis B <bluz71@users.noreply.github.com>
2017-04-01 17:51:50 +1100

    Handy shortcuts for IEx including Repo.first and Repo.last

---
+++ .iex.exs
@@ -0,0 +1,6 @@
+import_file "~/.iex.exs"
+
+alias Discuss.Repo
+alias Discuss.Topic
+alias Discuss.User
+alias Discuss.Comment
--- lib/discuss/repo.ex
+++ lib/discuss/repo.ex
@@ -1,3 +1,15 @@
 defmodule Discuss.Repo do
   use Ecto.Repo, otp_app: :discuss
+
+  # Handy Repo helper functions via:
+  #
+  # https://www.grok-interactive.com/blog/ruby-rails-vs-elixir-phoenix
+
+  def first(queryable) do
+    queryable |> Ecto.Query.first |> one
+  end
+
+  def last(queryable) do
+    queryable |> Ecto.Query.last |> one
+  end
 end
